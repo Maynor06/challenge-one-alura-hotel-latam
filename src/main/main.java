a96cb69ce16b99f157b23f3b1e6f226b19b67b5b
Maynor <dmaynor488@gmail.com>
2023-10-03 19:17:31 -0600

    Haciendo configuraciones iniciales

---
+++ src/main/main.java
@@ -0,0 +1,14 @@
+package main;
+
+import views.MenuPrincipal;
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+		MenuPrincipal menuPrincipal = new MenuPrincipal(); 
+		
+		
+	}
+
+}
